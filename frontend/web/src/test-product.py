34c6828481aafbe43a6758d2f8ffbbdb007a5fb3
editaylors7 <gphpqocr674@hotmail.com>
2025-12-05 01:20:20 +0800

    add core.js, export.md, UniversalAdapter.ts

---
+++ frontend/web/src/test-product.py
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\frontend\web\src\test-product.py
+// Generated at: 2025-10-18T05:04:38.408Z
---
+++ inventory/analytics-provider-advanced/core.js
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export const adapter-core = () => { return null; };
